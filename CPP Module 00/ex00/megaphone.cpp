3bcc9d213515d7902ff86edbeec8c490b1f9ed7b
2winyear <lsy2246@naver.com>
2022-02-17 01:47:16 +0900

    finish CPP Module 00 ex00

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
 
-//https://modoocode.com/172
 int main(int argc, char **argv)
 {
-
+    if (argc > 1)
+    {
+		for(int i = 1; i < argc; i++)
+		{
+			for (int j = 0; j < strlen(argv[i]); j++)
+				std::cout << (char)toupper(argv[i][j]);
+			std::cout << " ";
+		}	
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << std::endl;
+	return (0);
 }
