992c00b9d22c22c65add4ae5e4441f60cf4ad810
2winyear <lsy2246@naver.com>
2022-02-21 21:33:01 +0900

    ex01

---
+++ CPP Module 01/ex01/ZombieHorde.cpp
@@ -0,0 +1,3 @@
+#include "Zombie.hpp"
+
+Zombie* zombieHorde(int N, std::string name);
--- CPP Module 01/ex01/main.cpp
+++ CPP Module 01/ex01/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	int num;
-	std::string name = ""
+	std::string name = "";
 	
 	std::cout << "How many Zombies do you want?" << std::endl;
 	getline(std::cin, num);
