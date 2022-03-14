7d35e49376931b24176d1038440576fdd141f3b6
2winyear <lsy2246@naver.com>
2022-03-14 14:41:45 +0900

    [ex03] inter

---
+++ CPP Module 05/ex03/Intern.hpp
@@ -0,0 +1,15 @@
+#ifndef INTERN_HPP
+#define INTERN_HPP
+
+#include "Form.hpp"
+
+class Intern {
+	private:
+
+	public:
+		Intern() {}
+		Intern(Intern const &) {}
+		Intern &operator=(Intern const &)
+};
+
+#endif
