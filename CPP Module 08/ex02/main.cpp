b590ef617783606fb057941cde38010f6ce4b46c
2winyear <lsy2246@naver.com>
2022-03-24 16:15:44 +0900

    add subject.pdf

--- CPP Module 08/ex02/main.cpp
+++ CPP Module 08/ex02/main.cpp
@@ -3,20 +3,31 @@
 int main(void)
 {
 	MutantStack<int> mstack;
-	mstack.push(9);
-	mstack.push(7);
+	mstack.push(5);
+	mstack.push(17);
+
 	std::cout << "top : " << mstack.top() << std::endl;
+	
 	mstack.pop();
+	
 	std::cout << "size : " << mstack.size() << std::endl;
+	
 	mstack.push(3);
 	mstack.push(5);
+	mstack.push(737);
 	mstack.push(0);
+	
 	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
 
-	while (it != mstack.end())
+	++it;
+	--it;
+
+	while (it != ite)
 	{
 		std::cout << "m stack : " << *it << std::endl;
 		++it;
 	}
+	std::stack<int> s(mstack);
 	return 0;
 }
