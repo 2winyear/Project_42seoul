417d11c1e776432138336d457335b7809a4d2186
2winyear <lsy2246@naver.com>
2022-05-12 21:56:43 +0900

    5/12 inception

--- Inception/srcs/requirements/mariadb/script.sh
+++ Inception/srcs/requirements/mariadb/script.sh
@@ -7,27 +7,9 @@ if [ ! -d /var/lib/mysql/$DB_NAME ]; then
   service mysql start
   apt-get install -y gettext-base
   envsubst < /tmp/create_mysql_db.sql | mysql
-  ###
-#   mysqladmin --user=root --password=$DB_ROOT_PASSWORD
   service mysql stop
 fi
 
 chown -R mysql:mysql /var/lib/mysql
 
 mysqld_safe
-
-# service mysql start
-
-# db_exist=$(echo "SHOW DATABASES" | mysql -u root | grep "wp_db" | wc -l)
-# echo "CREATE DATABASE wp_db;" | mysql -u root
-# echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -u root
-# echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'" | mysql -u root
-# echo "FLUSH PRIVILEGES" | mysql -u root
-
-# if [ $db_exist = "0" ]
-# then
-# 	mysql wp_db -u root < /tmp/wp-saved.sql
-# fi
-# service mysql stop
-
-# exec /usr/sbin/mysqld -u root
--- Inception/srcs/requirements/wordpress/script.sh
+++ Inception/srcs/requirements/wordpress/script.sh
@@ -1,47 +1,3 @@
-# set -e
-
-# if [ -f '/etc/php7/php-fpm.d/www.conf' ]; then
-# 	echo "configuring php-fpm..."
-# 	cat /etc/php7/php-fpm.d/www.conf \
-# 	| sed 's/127.0.0.1:9000/9000/' \
-# 	| sed 's/;clear_env = no/clear_env = no/' > /etc/php7/php-fpm.d/wp.conf
-	
-# 	rm /etc/php7/php-fpm.d/www.conf
-# fi
-
-# # if [ -d '/var/www/html/wordpress' ]; then
-# # 	echo "wordpress has been found installed in mounted volume."
-# # else
-# # 	echo "copying initial wordpress data."
-# # 	cp $(find / -name wp-config-docker.php) /wordpress/wp-config.php
-# # 	cp -r /wordpress /var/www/html
-# # fi
-
-# chown -R www-data:www-data /var/www/html
-
-# echo "wordpress has been setted up."
-
-# # exec "$@"
-
-# # !bin/bash/
-
-# # 	Download wordpress compressed file, decompress it,
-# # 	move the dir to the right emplacement and delete .tar file
-
-# # mv /tmp/wp-config.php /var/www/wordpress
-# # chown -R www-data:www-data /var/www/wordpress/
-
-# # 	Set up daemonize to 'no' in the php-fpm.conf so i can run it properly after
-
-# # sed -i -e "s|MYSQL_DATABASE|'$MYSQL_DATABASE'|g" /var/www/wordpress/wp-config.php
-# # sed -i -e "s|MYSQL_USER|'$MYSQL_USER'|g" /var/www/wordpress/wp-config.php
-# # sed -i -e "s|MYSQL_PASSWORD|'$MYSQL_PASSWORD'|g" /var/www/wordpress/wp-config.php
-# # sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
-# # mkdir -p /run/php/
-
-# exec /usr/sbin/php-fpm7.3 -F
-
-
 #!bin/bash/
 mkdir -p /run/php/
 
@@ -66,10 +22,10 @@ wp core install		--allow-root \
 					--admin_email=$WP_ADMIN_EMAIL \
 					--path=/var/www/wordpress
 
-#wp user create bob	"$WP_REG_USER@$WP_URL" \
-#					--allow-root \
-#					--role=author \
-#					--user_pass=$WP_REG_USER_PASSWORD \
-#					--path=/var/www/wordpress
+#wp user create seungyel	"$WP_REG_USER@$WP_URL" \
+#							--allow-root \
+#							--role=author \
+#							--user_pass=$WP_REG_USER_PASSWORD \
+#							--path=/var/www/wordpress
 
 /usr/sbin/php-fpm7.3 -F --nodaemonize
