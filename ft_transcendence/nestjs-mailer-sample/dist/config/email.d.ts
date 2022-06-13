45f4231e26d19d19491ee312c0ee2eb1bbb15586
2winyear <lsy2246@naver.com>
2022-06-14 02:13:10 +0900

    email finish

---
+++ ft_transcendence/nestjs-mailer-sample/dist/config/email.d.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+declare const _default: () => {
+    email: {
+        transport: string;
+        defaults: {
+            from: string;
+        };
+    };
+};
+export default _default;
